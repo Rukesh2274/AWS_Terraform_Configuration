594908d9c5967a2ba3d8c92db2788fbfae2a7757
Tanguturi Rukesh Reddy <90524890+Rukesh2274@users.noreply.github.com>
2023-09-02 10:38:23 +0530

    terraform config

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+
+  region     = "us-east-1"
+  access_key = "AKIA5WNG3K37B73564MS"
+  secret_key = "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"
+
+}
